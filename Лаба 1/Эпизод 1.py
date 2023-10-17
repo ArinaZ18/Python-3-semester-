90d6d8fc686301151385a154becea8af69913e16
Arina Zholnerovich <114029905+ArinaZ18@users.noreply.github.com>
2023-10-17 16:05:37 +0300

    Update and rename Эпизод 1 to Эпизод 1.py

--- Лаба 1/Эпизод 1.py
+++ Лаба 1/Эпизод 1.py
@@ -9,9 +9,29 @@ for i in range(len(arr)):
         x,y = f.readline().split()
         X.append(float(x))
         Y.append(float(y))
+    
+    w1=sorted(X)
+    w2=sorted(Y)
+  
+    q1=[]
+    q2=[]
+    for s in range(len(X)-1):
+      #print(w1[s], w1[s+1], abs(w1[s]-w1[s+1]))
+      if (abs(w1[s]-w1[s+1])!=0):
+        q1.append(abs(w1[s]-w1[s+1]))
+      if (abs(w2[s]-w2[s+1])!=0):
+        q2.append(abs(w2[s]-w2[s+1]))
+  
+    m1=min(q1)
+    m2=min(q2)
+    m=min(m1,m2) * 2
+
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
-    plt.scatter(X,Y)
+    plt.scatter(X,Y,s=m)
     plt.title('Number of points: ' + str(len(X)))
     plt.axis('scaled')
+    plt.show()
     plt.savefig('Эпизод 1'+ 'Number of points: ' + str(len(X)))
+
+
